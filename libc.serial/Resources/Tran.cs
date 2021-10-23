f302206900eca1c5db6ec88db351d419e6f4c12c
Saeid Farahi <saeidfarahi@gmail.com>
2021-10-23 15:35:39 +0330

    Updated deps

--- libc.serial/Resources/Tran.cs
+++ libc.serial/Resources/Tran.cs
@@ -3,10 +3,15 @@ using libc.translation;
 
 namespace libc.serial.Resources
 {
+
     internal static class Tran
     {
-        public static readonly ILocalizer Instance = new Localizer(new LocalizationSource(
-            Assembly.GetExecutingAssembly(),
-            $"{typeof(Tran).Namespace}.tran.i18n.json", LocalizationSourcePropertyCaseSensitivity.CaseInsensitive));
+        public static readonly ILocalizer Instance = new Localizer(
+            new JsonLocalizationSource(
+                Assembly.GetExecutingAssembly(),
+                $"{typeof(Tran).Namespace}.tran.i18n.json", PropertyCaseSensitivity.CaseInsensitive
+            )
+        );
     }
+
 }
